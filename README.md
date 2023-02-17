9a875177dec776f8ea5d41e00c51d0c08f5633b9
Mohamed Aatif <mhmd.aatif2@gmail.com>
2023-02-17 22:08:10 +0400

    Added service-worker

--- README.md
+++
@@ -1,5 +0,0 @@
-# CW1-Individual
-
-github repo: https://github.com/WebXoX/CW1-Individual/
-
-github pages: https://webxox.github.io/CW1-Individual/
---
+++ service-worker.js
@@ -0,0 +1,28 @@
+var cacheName = "activitystore-v1";
+var cacheFiles = ["index.html", "style", "webstore.webmanifest", "js", "image"];
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});

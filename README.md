376536de05f83a4ed0a1d360adba73f2eb2a7262
Mohamed Aatif <mhmd.aatif2@gmail.com>
2023-02-17 22:06:10 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CW1-Individual
+
+github repo: https://github.com/WebXoX/CW1-Individual/
+
+github pages: https://webxox.github.io/CW1-Individual/
---
+++ js/lessons.js
@@ -0,0 +1,112 @@
+let lessons =[
+    {
+        id: 1001,
+        title: "Math",
+        location: "London",
+        quantity: 5,
+        price:  300,
+        image: "image/math.png",
+        rating: 5,
+        show:true
+    },
+     {
+        id: 1002,
+        title: "Science",
+        location: "Dubai",
+        quantity: 5,
+        price:  130,
+        image: "image/science.png",
+        rating: 5,
+        show:true
+    },
+     {
+        id: 1003,
+        title: "SST",
+        location: "London",
+        quantity: 5,
+        price:  20,
+        image: "image/SST.png",
+        rating: 1,
+        show:true
+    },
+    {
+        id: 1004,
+        title: "Mobile Dev",
+        location: "London",
+        quantity: 5,
+        price:  120,
+        image: "image/mobile.png",
+        rating: 4,
+        show:true
+    },
+    {
+        id: 1005,
+        title: "Web Dev",
+        location: "London",
+        quantity: 5,
+        price:  212,
+        image: "image/web.png",
+        rating: 4,
+        show:true
+    }, {
+        id: 1006,
+        title: "English",
+        location: "Kerala",
+        quantity: 5,
+        price:  300,
+        image: "image/english.png",
+        rating: 5,
+        show:true
+    },
+     {
+        id: 1007,
+        title: "Law",
+        location: "London",
+        quantity: 5,
+        price:  100,
+        image: "image/law.png",
+        rating: 5,
+        show:true
+    },
+     {
+        id: 1008,
+        title: "Dance",
+        location: "Dubai",
+        quantity: 5,
+        price:  201,
+        image: "image/dance.png",
+        rating: 1,
+        show:true
+    },
+    {
+        id: 1009,
+        title: "Music",
+        location: "Dubai",
+        quantity: 5,
+        price:  105,
+        image: "image/music.png",
+        rating: 4,
+        show:true
+    },
+    {
+        id: 10010,
+        title: "Cartography ",
+        location: "Hogwarts",
+        quantity: 5,
+        price:  224,
+        image: "image/mapping.png",
+        rating: 4,
+        show:true
+    }
+    , {
+        id: 10011,
+        title: "French ",
+        location: "Hogwarts",
+        quantity: 5,
+        price:  200,
+        image: "image/french.png",
+        rating: 4,
+        show:true
+    }
+
+]

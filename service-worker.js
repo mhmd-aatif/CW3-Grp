d1509ac73d82aa2293347335eac6a468b982a9c3
Mohamed Aatif <mhmd.aatif2@gmail.com>
2023-02-17 23:44:02 +0400

    Changes in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 var cacheName = "activitystore-v1";
-var cacheFiles = [
+var Files = [
   "index.html",
   "stylesheets",
   "images",
@@ -12,7 +12,7 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
       console.log("[Service Worker] Caching all the files");
-      return cache.addAll(cacheFiles);
+      return cache.addAll(Files);
     })
   );
 });

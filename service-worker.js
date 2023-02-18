d69dbb9095d864c81b6dfe83c5740c13705c8648
Mohamed Aatif <mhmd.aatif2@gmail.com>
2023-02-18 13:10:09 +0400

    service worker changes

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,34 @@
-var cacheName = "activitystore-v2";
-var files = [
-  "index.html",
-  "lessons.js",
-  "style.css",
-  "webstore.webmanifest",
-  "images",
+var cacheName = 'activities-v1';
+var cacheFiles = [
+    'index.html',
+	 'style.css',
+    'lessons.js',
+    'webstore.webmanifest',
+    'images',
 ];
 
-self.addEventListener("install", (e) => {
-  console.log("[Service Worker] Install");
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log("[Service Worker] Caching all the files");
-      return cache.addAll(files);
-    })
-  );
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (r) {
-      return (
-        r ||
-        fetch(e.request).then(function (response) {
-          return caches.open(cacheName).then(function (cache) {
-            cache.put(e.request, response.clone());
-            return response;
-          });
+// cache the third-party files
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
-      );
-    })
-  );
+    );
 });

fb4106075eb76ad437c3c2cef76470e935853210
Mohamed Aatif <mhmd.aatif2@gmail.com>
2023-02-18 13:55:59 +0400

    Changes

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,37 @@
-var cacheName = 'activities-v1';
+var cacheName = "activities-v1";
 var cacheFiles = [
-    'index.html',
-	 'style.css',
-    'lessons.js',
-    'webstore.webmanifest',
-    'images',
+  "index.html",
+  "style.css",
+  "lessons.js",
+  "webstore.webmanifest",
+  "images",
 ];
 
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all the files');
-            return cache.addAll(cacheFiles);
-        })
-    );
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
+    })
+  );
 });
 
 // cache the third-party files
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (r) {
-            // Download the file if it is not in the cache,
-            return r || fetch(e.request).then(function (response) {
-                // add the new file to cache
-                return caches.open(cacheName).then(function (cache) {
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      // Download the file if it is not in the cache,
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          // add the new file to cache
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            return response;
+          });
         })
-    );
+      );
+    })
+  );
 });
